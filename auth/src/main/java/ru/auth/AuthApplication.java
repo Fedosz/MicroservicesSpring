abbb39d9049dc8a09851a5f9969db4f1cc776108
Fedosz <fedya.zhukov.2014@mail.ru>
2023-05-23 20:38:26 +0300

    added db and project files

---
+++ auth/src/main/java/ru/auth/AuthApplication.java
@@ -0,0 +1,14 @@
+package ru.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.*;
+
+@SpringBootApplication
+public class AuthApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
